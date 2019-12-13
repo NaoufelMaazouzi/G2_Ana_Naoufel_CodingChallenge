7df941d9527202b39da253810709c1491f7bbf56
anadarocha <48509215+anadarocha@users.noreply.github.com>
2019-12-13 17:19:19 +0100

    ajout ana

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+function error(){
+		var t = document.form.username.value;
+		if(t == "") {
+		   alert('Le champ ne peut pas rester vide !! Veuillez introduire votre formule de calcul');
+ 
+		  }
+		  else {
+			alert("rempli");  
+		  } 
+		}
